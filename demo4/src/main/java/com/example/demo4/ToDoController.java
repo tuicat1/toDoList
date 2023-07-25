ad9fd0f0afab27242571840595f46bb63e33fa4f
Austen Edge <austen.edge@xtra.co.nz>
2023-07-25 14:47:11 +1200

    feat: add editing capabilities to items in to do list

--- demo4/src/main/java/com/example/demo4/ToDoController.java
+++ demo4/src/main/java/com/example/demo4/ToDoController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -24,6 +25,10 @@ public class ToDoController implements Initializable {
         // Initialize the todoList with an ObservableList
         items = FXCollections.observableArrayList();
         todoList.setItems(items);
+
+        // Allow editing of items in the ListView
+        todoList.setEditable(true);
+        todoList.setCellFactory(listView -> new EditableListCell());
     }
 
     @FXML
@@ -37,6 +42,52 @@ public class ToDoController implements Initializable {
         int indexToRemove = todoList.getSelectionModel().getSelectedIndex();
         if (indexToRemove >= 0) {
             items.remove(indexToRemove);
+        }
+    }
+
+    // Custom ListCell for editing the items
+    private class EditableListCell extends javafx.scene.control.ListCell<String> {
+        private TextField textField;
+
+        public EditableListCell() {
+            // Create a text field to handle edits
+            textField = new TextField();
+            textField.setOnAction(e -> commitEdit(textField.getText()));
+            textField.focusedProperty().addListener((obs, wasFocused, isNowFocused) -> {
+                if (!isNowFocused) {
+                    commitEdit(textField.getText());
+                }
+            });
+        }
+
+        @Override
+        protected void updateItem(String item, boolean empty) {
+            super.updateItem(item, empty);
+            if (empty) {
+                setText(null);
+                setGraphic(null);
+            } else {
+                setText(item);
+                setGraphic(null);
+            }
+        }
+
+        @Override
+        public void startEdit() {
+            super.startEdit();
+            String text = getItem();
+            if (text != null) {
+                textField.setText(text);
+            }
+            setGraphic(textField);
+            setText(null);
+        }
+
+        @Override
+        public void cancelEdit() {
+            super.cancelEdit();
+            setText(getItem());
+            setGraphic(null);
         }
     }
 }
